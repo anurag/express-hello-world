a6c116f390a9efb932d639ae69c8668c119d61e6
Anurag Goel <59905+anurag@users.noreply.github.com>
2021-01-27 15:28:47 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello Gary and Alex!"));
+app.get("/", (req, res) => res.send("Hello Alex!"));
 
 app.listen(port, () => 
   console.log(`Example app started on port ${port}`)
